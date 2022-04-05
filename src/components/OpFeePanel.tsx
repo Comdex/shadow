bb29d12267264973e2d00aac9d5ff111c9fb0cf0
coldstar1993 <coldstar1993@163.com>
2022-04-06 00:30:58 +0800

    get amountInput from parent

--- src/components/OpFeePanel.tsx
+++ src/components/OpFeePanel.tsx
@@ -2,36 +2,42 @@ import * as React from 'react';
 import { Modal, Button, Input, Typography } from 'antd';
 import styles from './index.module.less'
 import styled from 'styled-components'
+import { ShadowContext } from '@/context/ShadowContext';
 
 const divStyles = { display: 'flex', justifyContent: 'space-between' }
 
-export const OpFeePanel = () => {
+export const OpFeePanel = (props) => {
+  let shadowData = React.useContext(ShadowContext);
 
-  const populateAmount = () => {
+  let { amount } = props;
+  console.log('fresh OpFeePanel...amount=', amount);
 
-  }
+  let layer1Fee = shadowData.layer1FeePersent * amount;
+  let layer2Fee = amount ? shadowData.layer2FeeFixed : 0;
+  let totalFee = amount ? layer1Fee + layer2Fee : 0;
+  let finalRecieve = amount - totalFee;
 
   return <div className={styles.withdrawFeePanel}>
     <div style={divStyles}>
       <div>Layer2 network fee</div>
-      <div>0.04%</div>
+      <div>{layer2Fee} Mina</div>
     </div>
     <div style={divStyles}>
       <div>Layer1 network fee</div>
-      <div>0.01521 Mina</div>
+      <div>{layer1Fee} Mina</div>
     </div>
     <div style={divStyles}>
       <div>total fee</div>
-      <div>0.01539 Mina</div>
+      <div>{totalFee} Mina</div>
     </div>
     <div style={divStyles}>
       <div>to revieve</div>
-      <div>0.18461 Mina</div>
+      <div>{finalRecieve} Mina</div>
     </div>
     <br />
     <div style={divStyles}>
       <div>Total</div>
-      <div>0.2 Mina</div>
+      <div>{amount} Mina</div>
     </div>
   </div>
 }
